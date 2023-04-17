4fd3fc9d7df825eba47e81f59653fb921034117d
Daksh2356 <dakshmakhija@gmail.com>
2023-04-18 01:24:29 +0530

    card added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,14 @@
+import Card from "../components/Card";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <div>Cards :</div>
+      <Card />
+    </div>
+  );
 };
 
 export default Home;
