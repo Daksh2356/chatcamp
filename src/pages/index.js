b80f9a1de1eb90320f2606c323b532ab4d0f9ae6
Unnat Das <udas4153@gmail.com>
2023-04-18 01:04:07 +0530

    added first time

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 // import Image from "next/image";
 // import { Inter } from "next/font/google";
 
+import ContactUs from "@/components/ContactUs";
+
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>CHATCAMP</div>;
+  return <div className=" w-full h-screen">
+  <div>ChatCamp</div>
+  <ContactUs/>
+  </div>;
 }
